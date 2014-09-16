110607e45e025cca774ebbc452f8e30ae92f29d8
Rob Prouse <rob@prouse.org>
2014-09-16 15:33:25 -0400

    Getting some of the event handling hooked up

--- app/src/main/java/net/alteridem/tipcalculator/TipActivity.java
+++ app/src/main/java/net/alteridem/tipcalculator/TipActivity.java
@@ -2,10 +2,15 @@ package net.alteridem.tipcalculator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,28 +18,59 @@ import java.util.List;
 
 public class TipActivity extends Activity {
 
+    private TextView _bill;
+    private Spinner percents;
+    private Spinner split;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tip);
 
+        _bill = (TextView)findViewById(R.id.activity_tip_bill_amount);
+        _bill.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                recalculate();
+            }
+        });
+
         List<String> percents_list = new ArrayList<String>(26);
         for(int i=0; i<=25; i++) {
             percents_list.add(String.format("%d%%", i));
         }
-
-        Spinner percents = (Spinner) findViewById(R.id.activity_tip_percent);
-        ArrayAdapter<String> percents_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, percents_list);
-        percents.setAdapter(percents_adapter);
+        percents = getSpinner(R.id.activity_tip_percent, percents_list, 15);
 
         List<String> split_list = new ArrayList<String>(12);
         for(int i=1; i<=12; i++) {
             split_list.add(String.format("%d", i));
         }
+        split = getSpinner(R.id.activity_tip_split, split_list, 0);
+    }
 
-        Spinner split = (Spinner) findViewById(R.id.activity_tip_split);
-        ArrayAdapter<String> split_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, split_list);
-        split.setAdapter(split_adapter);
+    private Spinner getSpinner(int spinnerId, List<String> list, int initialSelection) {
+        Spinner spinner = (Spinner) findViewById(spinnerId);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        spinner.setAdapter(adapter);
+        spinner.setSelection(initialSelection);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                recalculate();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                recalculate();
+            }
+        });
+        return spinner;
     }
 
     @Override
@@ -57,5 +93,9 @@ public class TipActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void recalculate() {
+
     }
 }
